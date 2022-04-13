8451189c6f455dded82197f9350656412e244dc5
Malik Wali <defdotpy@gmail.com>
2022-04-13 17:28:06 +0700

    functrace cockroach-db

--- cockroach-db.sh
+++ cockroach-db.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -eE -o functrace
+
+failure() {
+    local statuscode=$? ; local lineno=$1 ; local msg=$2
+    echo "Failed with exit code ($statuscode) at line ($lineno) => $msg"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
 
 mkdir -p $APPLICATION_DIR
 cd $APPLICATION_DIR || exit 1
