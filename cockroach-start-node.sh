945648c06bd1d08b3ef11f7624787e5dc8d25517
Malik Wali <defdotpy@gmail.com>
2022-04-17 15:54:23 +0700

    cockroach start-node

--- cockroach-start-node.sh
+++ cockroach-start-node.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
+set -eE -o functrace
+
+failure() {
+    local statuscode=$? ; local lineno=$1 ; local msg=$2
+    echo "Failed with exit code ($statuscode) at line ($lineno) => $msg"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 
 # Intalling CockroachDB
 curl -O https://binaries.cockroachdb.com/cockroach-v21.2.7.linux-amd64.tgz
 tar -xzvf cockroach-v21.2.7.linux-amd64.tgz
+sudo rm -f /usr/local/bin/cockroach
 sudo cp -i cockroach-v21.2.7.linux-amd64/cockroach /usr/local/bin/
 sudo mkdir -p /usr/local/lib/cockroach
+sudo rm -f /usr/local/lib/cockroach/libgeos.so
 sudo cp -i cockroach-v21.2.7.linux-amd64/lib/libgeos.so /usr/local/lib/cockroach/
+sudo rm -f /usr/local/lib/cockroach/libgeos_c.so
 sudo cp -i cockroach-v21.2.7.linux-amd64/lib/libgeos_c.so /usr/local/lib/cockroach/
 
 
 cockroach start \
---certs-dir=$APPLICATION_DIR/$NODE_NAME/certs \
+--certs-dir=$APPLICATION_DIR/$NODE_NAME//certs \
 --advertise-addr=$NODE_IP \
 --join=$NODE_JOIN \
 --cache=.25 \
---max-sql-memory=.25 \
---background
+--max-sql-memory=.25
