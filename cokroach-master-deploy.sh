728f62982702085645951ae3b27ddec361fded55
Malik Wali <defdotpy@gmail.com>
2022-04-18 15:41:46 +0700

    cockroach

---
+++ cokroach-master-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eE -o functrace
+
+failure() {
+    local statuscode=$? ; local lineno=$1 ; local msg=$2
+    echo "Failed with exit code ($statuscode) at line ($lineno) => $msg"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+sudo cockroach init --certs-dir=certs --host=$NODE_IP
