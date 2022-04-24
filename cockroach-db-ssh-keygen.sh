e130553bfb407127e61393dfd1d2a59989743135
Malik Wali <defdotpy@gmail.com>
2022-04-24 15:09:11 +0700

    cockroach

--- cockroach-db-ssh-keygen.sh
+++ cockroach-db-ssh-keygen.sh
@@ -8,13 +8,5 @@ failure() {
 
 trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 
-# Sync Time
-sudo timedatectl set-ntp no
-sudo apt-get install -qq ntp
-sudo service ntp stop
-sudo ntpd -b time.google.com
-sudo service ntp start
-sudo ntpq -p
-
 #One time
 sudo -u nomad ssh-keygen -t rsa -N "" -f /etc/nomad.d/.ssh/id_rsa
