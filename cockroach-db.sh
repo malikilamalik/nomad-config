ea6f79beccc48e73d2a34b2b5d71cfb4516f2c33
Malik Wali <defdotpy@gmail.com>
2022-05-16 22:20:17 +0700

    feat: nginx config

--- cockroach-db.sh
+++ cockroach-db.sh
@@ -17,14 +17,14 @@ sudo apt-get install -qq sshpass
 
 # Intalling CockroachDB
 curl -O https://binaries.cockroachdb.com/cockroach-v21.2.10.linux-amd64.tgz
-tar -xzvf cockroach-v21.3.7.linux-amd64.tgz
+tar -xzvf cockroach-v21.2.10.linux-amd64.tgz
 sudo rm -f /usr/local/bin/cockroach
-sudo cp -i cockroach-v21.3.7.linux-amd64/cockroach /usr/local/bin/
+sudo cp -i cockroach-v21.2.10.linux-amd64/cockroach /usr/local/bin/
 sudo mkdir -p /usr/local/lib/cockroach
 sudo rm -f /usr/local/lib/cockroach/libgeos.so
-sudo cp -i cockroach-v21.3.7.linux-amd64/lib/libgeos.so /usr/local/lib/cockroach/
+sudo cp -i cockroach-v21.2.10.linux-amd64/lib/libgeos.so /usr/local/lib/cockroach/
 sudo rm -f /usr/local/lib/cockroach/libgeos_c.so
-sudo cp -i cockroach-v21.3.7.linux-amd64/lib/libgeos_c.so /usr/local/lib/cockroach/
+sudo cp -i cockroach-v21.2.10.linux-amd64/lib/libgeos_c.so /usr/local/lib/cockroach/
 
 # Make Cert Directory
 rm -drf certs
